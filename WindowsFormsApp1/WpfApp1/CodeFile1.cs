2772ff6301c1f8ac68593c06b7c2337285f5dd9e
Nikole-Scheutz <nikolai.scheutz@elev.ga.ntig.se>
2022-11-24 15:21:21 +0100

    Remove yml file
    Co-Authored-By: VincentKarm <105645043+VincentKarm@users.noreply.github.com>
    Co-Authored-By: Hilmir Brynjolfsson <111963441+Hilmir4@users.noreply.github.com>
    Co-Authored-By: Nikole Scheutz <112065881+nikole-scheutz@users.noreply.github.com>
    Co-Authored-By: Axelgustavschnurer <111983315+Axelgustavschnurer@users.noreply.github.com>
    Co-Authored-By: RoderXD <111990906+RoderXD@users.noreply.github.com>

---
+++ WindowsFormsApp1/WpfApp1/CodeFile1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Program.EnableVisualStyles();
+            Program.SetCompatibleTextRenderingDefault(false);
+            Program.Run(new Form1());
+        }
+    }
+}
