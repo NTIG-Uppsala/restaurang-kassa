16f0cfffc531f8e4f7a70863b52c6c6b5d930ff8
Axel Schnürer <axel.schnurer@elev.ga.ntig.se>
2022-12-01 11:47:23 +0100

    Created projects

---
+++ restaurant-pos/restaurant-pos/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace restaurant_pos;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ restaurant-pos/restaurant-pos/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace restaurant_pos;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
