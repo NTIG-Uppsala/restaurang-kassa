9732964f21b32f94791f51eda5db99a2d5551e6b
Nikole-Scheutz <nikolai.scheutz@elev.ga.ntig.se>
2022-12-12 10:05:24 +0100

    start creation of cli application
    Co-Authored-By: VincentKarm <105645043+VincentKarm@users.noreply.github.com>
    Co-Authored-By: RoderXD <111990906+RoderXD@users.noreply.github.com>
    Co-Authored-By: Hilmir Brynjolfsson <111963441+Hilmir4@users.noreply.github.com>
    Co-Authored-By: Axelgustavschnurer <111983315+Axelgustavschnurer@users.noreply.github.com>
    Co-Authored-By: Nikole Scheutz <112065881+nikole-scheutz@users.noreply.github.com>

---
+++ restaurant-pos/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Backend
+{
+    class Test
+    {
+        public static void Main(string[] args)
+        {
+            string testString;
+            Console.Write("Enter a string - ");
+            testString = Console.ReadLine();
+            if (testString == "a")
+            {
+                Console.WriteLine("You selected option {0}", testString);
+            }
+            else
+            {
+                Console.WriteLine("You entered '{0}'", testString);
+            }
+
+        }
+    }
+}
