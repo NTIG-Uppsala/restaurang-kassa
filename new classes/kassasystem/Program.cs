2146d93b65370873ca4e25b49207ab85e2801d01
Linus J <linus.jansson2000@gmail.com>
2023-01-12 09:41:12 +0100

    Added descriptive comments
    Co-Authored-By: Hilmir Brynjolfsson <111963441+Hilmir4@users.noreply.github.com>
    Co-Authored-By: Nikole Scheutz <112065881+Nikole-Scheutz@users.noreply.github.com>
    Co-Authored-By: VincentKarm <105645043+VincentKarm@users.noreply.github.com>
    Co-Authored-By: RoderXD <111990906+RoderXD@users.noreply.github.com>

--- new classes/kassasystem/Program.cs
+++ new classes/kassasystem/Program.cs
@@ -1,152 +1,152 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-using System.Xml;
-
-class Cart
+﻿namespace kassasystem
 {
-    public List<Product> items = new List<Product>();
-
-    public decimal getTotalPrice()
+    class Cart
     {
-        decimal total = 0;
-        foreach (Product product in items)
+        public List<Product> items = new List<Product>();
+
+        public decimal getTotalPrice()
         {
-            total += product.getPrice();
+            /*
+                Sums up the price of all products and returns the total 
+            */
+
+            decimal total = 0;
+            foreach (Product product in items)
+            {
+                total += product.getPrice();
+            }
+
+            return total;
+
         }
 
-        return total;
-
-    }
-    public void addProduct(int productID, Menu productMenu)
-    {
-        foreach (Product entry in productMenu.menuItems)
+        public void addProduct(int productID, Menu productMenu)
         {
-            if (productID != entry.id) continue;
-            this.items.Add(entry);
-            Console.WriteLine("Added " + entry.id + " " + entry.name);
-            break;
+            /*
+                Method to add a product from the menu to the cart
+            */
+
+            // Loop over the menu
+            foreach (Product entry in productMenu.menuItems)
+            {
+                // If the product id that wants to be added is not the current item in the loop skip
+                if (productID != entry.id) continue;
+
+                this.items.Add(entry); // Add item to the cart
+                Console.WriteLine("Added " + entry.id + " " + entry.name);
+                break; // break out of the loop
+            }
         }
-    }
 
-    public void removeProduct(int productID) 
-    {
-        foreach (Product product in this.items)
+        public void removeProduct(int productID)
         {
+            /*
+                Removes a product from the cart     
+            */
 
-           if (productID == product.id)
-           {
+            foreach (Product product in this.items)
+            {
+
+                if (productID != product.id) continue;
+                
                 this.items.Remove(product);
                 Console.WriteLine("Removed " + product.id + " " + product.name);
                 break;
-           }
+                
+            }
+        }
+
+        public bool pay()
+        {
+            /*
+                Returns if the payment went through or not 
+            */
+
+            // In the future this could call a possible processPayment method
+
+            bool isPaid = true;
+            this.clearCart();
+
+            return isPaid;
+        }
+
+        public void clearCart()
+        {
+            this.items.Clear();
         }
     }
 
-    public bool pay()
+    class Product
     {
-        /*
-            Returns if the payment went through or not 
-        */
+        public int id;
+        public string name;
+        public string description;
+        public decimal price;
+        public decimal tax;
 
-        // In the future this could call a possible processPayment method
-
-        bool isPaid = true;
-        this.clearCart();
-
-        return isPaid;
-    }
-
-    public void clearCart()
-    {
-        this.items.Clear();
-    }
-}
-
-class Product
-{
-    public int id;
-    public string name;
-    public string description;
-    public decimal price;
-    public decimal tax;
-
-    public Product(int id, string name, string description, decimal price, decimal tax)
-    {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.tax = tax;
-    }
-
-    public decimal getPrice()
-    {
-        // Calculates price of product with tax
-        Console.WriteLine("Tax" + (1.0m + this.tax));
-        return this.price * (1.0m + this.tax);
-    }
-
-    public decimal getPriceNoTax()
-    {
-        // Calculates price of product without tax
-        return this.price;
-    }
-
-    public string getStringPrice()
-    {
-        return string.Format("{0} SEK", getPrice());
-    }
-
-}
-
-class Menu
-{
-    public List<Product> menuItems = new List<Product>();
-
-    public void addProduct(string name, string description, decimal price, decimal tax)
-    {
-        int last_avaliable_product_id = 0;
-        if (menuItems.Count > 0) 
+        public Product(int id, string name, string description, decimal price, decimal tax)
         {
-            int last_product_id = menuItems.Last().id;
-            last_avaliable_product_id = last_product_id + 1; 
+            this.id = id;
+            this.name = name;
+            this.description = description;
+            this.price = price;
+            this.tax = tax;
         }
-        
-        Product new_product = new Product(last_avaliable_product_id, $"Product {name}", $"Product {description} description",  price, tax);
-        menuItems.Add(new_product);
+
+        public decimal getPrice()
+        {
+            // Calculates price of product with tax eg (100 * 1.25 -> 125 SEK)
+            return this.price * (1.0m + this.tax);
+        }
+
+        public decimal getPriceNoTax()
+        {
+            // Calculates price of product without tax
+            return this.price;
+        }
+
+        public string getStringPrice()
+        {
+            /*
+                Returns a string with the correct format of the price eg 250.00 SEK
+            */
+            return string.Format("{0} SEK", getPrice());
+        }
+
     }
 
-    public void removeProduct(int id)
+    class Menu
     {
-        foreach (Product product in this.menuItems)
+        public List<Product> menuItems = new List<Product>();
+
+        public void addProduct(string name, string description, decimal price, decimal tax)
         {
-            if (product.id == id)
+            /*
+                Add a product to the menu 
+            */
+
+            // Gets id of last item in menu if menu has items and adds 1 to it. 
+            // If the menu has no items, just set the id to 0
+            int last_avaliable_product_id = 0;
+            if (menuItems.Count > 0)
             {
+                int last_product_id = menuItems.Last().id;
+                last_avaliable_product_id = last_product_id + 1;
+            }
+
+            Product new_product = new Product(last_avaliable_product_id, $"Product {name}", $"Product {description} description", price, tax);
+            menuItems.Add(new_product);
+        }
+
+        public void removeProduct(int id)
+        {
+            foreach (Product product in this.menuItems)
+            {
+                if (product.id != id) continue;
+                
                 menuItems.Remove(product);
                 break;
             }
         }
-    }
-}
-
-class Program
-{
-    static int Main(string[] args)
-    {
-        Menu menu = new Menu();
-
-        menu.addProduct("Bun", "Soft and nice", 15, 0.12m);
-        menu.addProduct("Coffee", "Black", 30, 0.12m);
-
-        foreach (Product entry in menu.menuItems)
-        {
-            Console.Write(entry.name + ", " + entry.getStringPrice() + ", ID: " + entry.id + "\n");
-        }
-        Cart cart = new Cart();
-        cart.addProduct(1, menu);
-        
-
-        return 0;
     }
 }
