2b680f16f9a0c14ca84f01ea1b505fa4d9071b8c
Linus J <linus.jansson2000@gmail.com>
2023-01-16 10:08:39 +0100

    Clean up repo from outdated code
    Co-Authored-By: Hilmir Brynjolfsson <111963441+Hilmir4@users.noreply.github.com>
    Co-Authored-By: VincentKarm <105645043+VincentKarm@users.noreply.github.com>
    Co-Authored-By: Nikole Scheutz <112065881+Nikole-Scheutz@users.noreply.github.com>
    Co-Authored-By: Linus J <30862662+linus-jansson@users.noreply.github.com>

--- restaurant-pos/restaurant-pos/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace restaurant_pos;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
