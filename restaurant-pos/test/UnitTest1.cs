16c2b583c94d7cd4a7fd103a98d30fc356998179
Nikole-Scheutz <nikolai.scheutz@elev.ga.ntig.se>
2023-01-23 12:48:05 +0100

    Add more tests
    Co-Authored-By: VincentKarm <105645043+VincentKarm@users.noreply.github.com>
    Co-Authored-By: Hilmir Brynjolfsson <111963441+Hilmir4@users.noreply.github.com>
    Co-Authored-By: RoderXD <111990906+RoderXD@users.noreply.github.com>
    Co-Authored-By: Leo Kempe <leo.max.kempe@gmail.com>

--- restaurant-pos/test/UnitTest1.cs
+++ restaurant-pos/test/UnitTest1.cs
@@ -1,9 +1,10 @@
+using System.Diagnostics;
 using Restaurant_pos_program;
 
-namespace test
+namespace Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class TestCart
     {
 
         [TestMethod]
@@ -12,8 +13,8 @@ namespace test
             Cart cart = new(1);
             Menu menu = new();
             // add product named "test" to menu
-            menu.AddProduct("test", "test description", 10, 1);
-            
+            menu.AddProduct("test", "test description", 10, 0.12m);
+
             // add the first product in menu to cart
             cart.AddProduct(0, menu);
 
@@ -33,7 +34,7 @@ namespace test
             // May need to be moved to a seperate method
             Cart cart = new(1);
             Menu menu = new();
-            menu.AddProduct("test", "test description", 10, 1);
+            menu.AddProduct("test", "test description", 10, 0.12m);
             cart.AddProduct(0, menu);
 
             Assert.AreEqual(10, cart.GetTotalPrice());
@@ -44,7 +45,7 @@ namespace test
         {
             Cart cart = new(1);
             Menu menu = new();
-            menu.AddProduct("test", "test description", 10, 1);
+            menu.AddProduct("test", "test description", 10, 0.12m);
             var cartItems = cart.GetCart();
             cart.AddProduct(0, menu);
 
@@ -59,7 +60,7 @@ namespace test
         {
             Cart cart = new(1);
             Menu menu = new();
-            menu.AddProduct("test", "test description", 10, 1);
+            menu.AddProduct("test", "test description", 10, 0.12m);
             var cartItems = cart.GetCart();
             cart.AddProduct(0, menu);
 
@@ -74,7 +75,7 @@ namespace test
         {
             Cart cart = new(1);
             Menu menu = new();
-            menu.AddProduct("test", "test description", 10, 1);
+            menu.AddProduct("test", "test description", 10, 0.12m);
             var cartItems = cart.GetCart();
             cart.AddProduct(0, menu);
             cart.AddProduct(0, menu);
@@ -91,14 +92,69 @@ namespace test
         {
             Cart cart = new(1);
             Menu menu = new();
-            menu.AddProduct("test", "test description", 10, 1);
-            menu.AddProduct("test2", "test description2", 20, 1);
+            menu.AddProduct("test", "test description", 10, 0.12m);
+            menu.AddProduct("test2", "test description2", 20, 0.12m);
             cart.AddProduct(0, menu);
             cart.AddProduct(1, menu);
             var cartItems = cart.GetCart();
             Assert.AreEqual(2, cartItems.Count);
             Assert.AreEqual("test", cartItems[0].name);
             Assert.AreEqual("test2", cartItems[1].name);
+        }
+    }
+
+    [TestClass]
+    public class TestMenu
+    {
+        [TestMethod]
+        public void TestAddProductMenu()
+        {
+            Menu menu = new();
+            menu.AddProduct("test", "test description", 10, 0.12m);
+            Assert.AreEqual("test", menu.GetMenu()[0].name);
+        }
+
+        [TestMethod]
+        public void TestGetMenu()
+        {
+            Menu menu = new();
+            menu.AddProduct("test", "test description", 10, 0.12m);
+            var menuItems = menu.GetMenu();
+            Assert.AreEqual("test", menuItems[0].name);
+            Assert.AreEqual("test description", menuItems[0].description);
+        }
+
+        [TestMethod]
+        public void TestRemoveProductMenu()
+        {
+            Menu menu = new();
+            menu.AddProduct("test", "test description", 10, 0.12m);
+            Assert.AreEqual(1, menu.GetMenu().Count());
+            menu.RemoveProduct(0);
+            Assert.AreEqual(0, menu.GetMenu().Count());
+        }
+    }
+
+    [TestClass]
+    public class TestProducts
+    {
+        [TestMethod]
+        public void TestGetPrice()
+        {
+            Menu menu = new();
+            menu.AddProduct("test", "test description", 10, 0.12m);
+
+            Assert.AreEqual(10, menu.GetMenu()[0].GetPrice());
+        }
+
+        [TestMethod]
+        public void TestGetPriceNoTax()
+        {
+            Menu menu = new();
+            menu.AddProduct("test", "test description", 10, 0.25m);
+            Debug.WriteLine(menu.GetMenu()[0].GetPriceNoTax());
+
+            Assert.AreEqual(8, menu.GetMenu()[0].GetPriceNoTax());
         }
     }
 }
