b33f85814e25ee73f88ec2c083a2ebbf0f79427b
Nikole-Scheutz <nikolai.scheutz@elev.ga.ntig.se>
2023-01-25 11:04:50 +0100

    Work in progress dynamic button generation
    Co-Authored-By: Hilmir Brynjolfsson <111963441+Hilmir4@users.noreply.github.com>
    Co-Authored-By: RoderXD <111990906+RoderXD@users.noreply.github.com>
    Co-Authored-By: VincentKarm <105645043+VincentKarm@users.noreply.github.com>

--- restaurant-pos/POS/MainWindow.xaml.cs
+++ restaurant-pos/POS/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Controls;
 using Restaurant_pos_program;
 
 namespace POS
@@ -8,10 +9,20 @@ namespace POS
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public class ProductButton : Button
+    {
+        public Product product;
+
+        public ProductButton(Product product)
+        {
+            this.product = product;
+        }
+    }
+
+public partial class MainWindow : Window
     {
         Cart cart = new Cart(1);
-        Menu menu = new Menu();
+        Restaurant_pos_program.Menu menu = new Restaurant_pos_program.Menu();
         Database database = new Database();
 
         public void LoadDatabaseProducts()
@@ -28,7 +39,53 @@ namespace POS
         {
             InitializeComponent();
             LoadDatabaseProducts();
+            additems();
         }
+
+        public void additems()
+        {
+            int i = 0;
+            foreach (Product product in menu.GetMenu())
+            {
+                var button = new ProductButton(product);
+                button.Content = product.name;
+                button.Name = $"button_{i.ToString()}";
+                
+                button.Click += updateCheckout;
+
+                itemButtonList.Items.Add(button);
+                i++;
+            }
+        }
+
+        private void updateCheckout(object sender, EventArgs e)
+        {
+
+            var button = sender as ProductButton;
+
+            if (button == null)
+            {
+                throw new NullReferenceException("sender is not ProductButton");
+            }
+            cart.AddProduct(Convert.ToInt64(button.Content), menu);
+        }
+
+        /*public void AddItemButtons()
+        {
+            int i = 0;
+            foreach (Product product in menu.GetMenu())
+            {
+
+                System.Windows.Controls.Button newBtn = new Button();
+
+                newBtn.Content = product.name;
+                newBtn.Name = "Button" + i.ToString();
+                newBtn.Click += new EventHandler(cart.AddProduct(i, menu));
+
+                itemButtonList.Items.Add(newBtn);
+                i++;
+            }
+        }*/
 
         private void AddCoffee(object sender, RoutedEventArgs e)
         {
@@ -58,5 +115,6 @@ namespace POS
             cart.Pay();
             listBox1.Items.Clear();
         }
+
     }
 }
