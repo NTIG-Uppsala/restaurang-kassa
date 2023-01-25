dcd5d7dbbca33aaf9594a2fca2722d3865862e23
Hilmir4 <hilmir.brynjolfsson@elev.ga.ntig.se>
2023-01-25 13:25:56 +0100

    fix dynamic button creation
    Co-Authored-By: Nikole Scheutz <112065881+Nikole-Scheutz@users.noreply.github.com>
    Co-Authored-By: VincentKarm <105645043+VincentKarm@users.noreply.github.com>

--- restaurant-pos/POS/MainWindow.xaml.cs
+++ restaurant-pos/POS/MainWindow.xaml.cs
@@ -67,53 +67,17 @@ public partial class MainWindow : Window
             {
                 throw new NullReferenceException("sender is not ProductButton");
             }
-            cart.AddProduct(Convert.ToInt64(button.Content), menu);
-        }
-
-        /*public void AddItemButtons()
-        {
-            int i = 0;
-            foreach (Product product in menu.GetMenu())
-            {
-
-                System.Windows.Controls.Button newBtn = new Button();
-
-                newBtn.Content = product.name;
-                newBtn.Name = "Button" + i.ToString();
-                newBtn.Click += new EventHandler(cart.AddProduct(i, menu));
-
-                itemButtonList.Items.Add(newBtn);
-                i++;
-            }
-        }*/
-
-        private void AddCoffee(object sender, RoutedEventArgs e)
-        {
-            cart.AddProduct(0, menu);
-            listBox1.Items.Add("Coffe");
+            cart.AddProduct(Convert.ToInt64(button.Name.TrimStart('b', 'u', 't', 'o', 'n', '_')), menu);
             totalPrice.Content = "Total Price: " + cart.GetTotalPrice();
+            listBox1.Items.Add(button.Content);
         }
-
-        private void AddBun(object sender, RoutedEventArgs e)
-        {
-            cart.AddProduct(1, menu);
-            listBox1.Items.Add("Bun");
-            totalPrice.Content = "Total Price: " + cart.GetTotalPrice();
-        }
-
-        private void AddThirdItem(object sender, RoutedEventArgs e)
-        {
-            cart.AddProduct(2, menu);
-            listBox1.Items.Add("ThirdITEM");
-            totalPrice.Content = "Total Price: " + cart.GetTotalPrice();
-        }
-
         private void Pay(object sender, RoutedEventArgs e)
         {
             Receipt receipt= new Receipt();
             receipt.CreateReceipt(cart);
             cart.Pay();
             listBox1.Items.Clear();
+            totalPrice.Content = "Total Price: " + cart.GetTotalPrice();
         }
 
     }
