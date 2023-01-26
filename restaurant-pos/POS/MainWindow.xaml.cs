147afbbd2f0ae479b76b745c72278130c3a8dc80
Linus J <linus.jansson2000@gmail.com>
2023-01-26 10:05:33 +0100

    Add scaling of elements to window size
    Co-Authored-By: Nikole Scheutz <112065881+Nikole-Scheutz@users.noreply.github.com>
    Co-Authored-By: RoderXD <111990906+RoderXD@users.noreply.github.com>
    Co-Authored-By: VincentKarm <105645043+VincentKarm@users.noreply.github.com>

--- restaurant-pos/POS/MainWindow.xaml.cs
+++ restaurant-pos/POS/MainWindow.xaml.cs
@@ -42,6 +42,55 @@ public partial class MainWindow : Window
             GenerateItemButtons();
         }
 
+        #region ScaleValue Depdency Property
+        public static readonly DependencyProperty ScaleValueProperty = DependencyProperty.Register("ScaleValue", typeof(double), typeof(MainWindow), new UIPropertyMetadata(1.0, new PropertyChangedCallback(OnScaleValueChanged), new CoerceValueCallback(OnCoerceScaleValue)));
+
+        private static object OnCoerceScaleValue(DependencyObject o, object value)
+        {
+            MainWindow mainWindow = o as MainWindow;
+            if (mainWindow != null)
+                return mainWindow.OnCoerceScaleValue((double)value);
+            else return value;
+        }
+
+        private static void OnScaleValueChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            MainWindow mainWindow = o as MainWindow;
+            if (mainWindow != null)
+                mainWindow.OnScaleValueChanged((double)e.OldValue, (double)e.NewValue);
+        }
+
+        protected virtual double OnCoerceScaleValue(double value)
+        {
+            if (double.IsNaN(value))
+                return 1.0f;
+
+            value = Math.Max(0.1, value);
+            return value;
+        }
+
+        protected virtual void OnScaleValueChanged(double oldValue, double newValue) { }
+
+        public double ScaleValue
+        {
+            get => (double)GetValue(ScaleValueProperty);
+            set => SetValue(ScaleValueProperty, value);
+        }
+        #endregion
+
+        private void CalculateScale()
+        {
+            double yScale = ActualHeight / 540f;
+            double xScale = ActualWidth / 960f;
+            double value = Math.Min(xScale, yScale);
+
+            //Debug.WriteLine($"SCALING:\n\tHeight:{ActualHeight} -> {yScale};\n\tWidth:{ActualWidth} -> {xScale};\n\tSome Value:{value} ");
+
+            ScaleValue = (double)OnCoerceScaleValue(POSMainWindow, value);
+        }
+
+        private void MainGrid_SizeChanged(object sender, SizeChangedEventArgs e) => CalculateScale(); // Scales MainWindow components to size of window 
+
         public void GenerateItemButtons()
         {
             int i = 0;
